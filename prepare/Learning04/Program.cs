fe77a706c09f69908c8b1902e398b74f4746fe0d
henriquegobi <henriquegobim@gmail.com>
2024-03-19 20:22:37 -0300

    W04 Prepare: Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using Learning04;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Create a base "Assignment" object
+        Assignment a1 = new Assignment("Elizabeth Alves", "Fisics");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Henrique Almeida", "programing", "2.3", "6-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeWorkList());
+
+        WritingAssignment a3 = new WritingAssignment("Claudiane Alves", "Human Resourses", "Employmnet");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,23 @@
+namespace Learning04
+{
+    public class WritingAssignment : Assignment
+    {
+        private string _title;
+
+        public WritingAssignment(string studentName, string topic, string title) :base(studentName, topic)
+        {
+            _title = title;
+        }
+        public string GetTile()
+        {
+            return _title;
+        }
+
+        public string GetWritingInformation()
+        {
+            string studentName = GetStudentName();
+            
+            return $"{_title} by {studentName}";
+        }
+    }
+}
